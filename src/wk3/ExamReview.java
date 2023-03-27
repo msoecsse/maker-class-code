c002b497fe26455ce17acb7340ae3475027ba654
dmaker-msoe <maker@msoe.edu>
2023-03-27 09:00:57 -0500

    Review stuff

--- src/wk3/ExamReview.java
+++ src/wk3/ExamReview.java
@@ -1,5 +1,8 @@
 package wk3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamReview {
 
     public static void bigOPractice(int[] list) {
@@ -7,6 +10,7 @@ public class ExamReview {
             helperFunctionToDivideByTwoThenConvertToInt(list);
         }
     }
+
     private static void helperFunctionToDivideByTwoThenConvertToInt(int[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             double divResult = ((double) list[i]) / 2;
@@ -19,7 +23,7 @@ public class ExamReview {
     public static void bigOPractice2(int[] list) {
         int i = 0;
 
-        while ( i < list.length) {
+        while (i < list.length) {
             System.out.print(i);
             i--;
         }
@@ -29,14 +33,46 @@ public class ExamReview {
         // n = length of list1
         // m = length of list2
 
-        for (int i = 0; i <list1.length - 1; i ++) {
+        for (int i = 0; i < list1.length - 1; i++) {
             for (int j = 0; j < list2.length - 1; j++) {
-                    System.out.print(i + j);
+                System.out.print(i + j);
             }
         }
 
         for (int j = 0; j < list2.length - 1; j++) {
             // Some constant time operation here
         }
+    }
+
+    public static double someMethodName(List<String> words) {
+        List<Integer> wordLengths = new ArrayList<>();
+        for (int i = 0; i < words.size() - 1; i++) {
+            if (i % 2 == 0) {
+                wordLengths.add(words.get(i).length());
+            }
+        }
+
+        double totalLength = 0.0;
+        for (Integer length : wordLengths) {
+            totalLength += length;
+        }
+
+        return totalLength / wordLengths.size();
+    }
+
+    public static <E extends Number> E findMax(List<E> numbers) {
+        E currentMax = null;
+
+        for (E number : numbers) {
+            if (currentMax == null || number.intValue() > currentMax.intValue()) {
+                currentMax = number;
+            }
+        }
+
+        return currentMax;
+    }
+
+    public <E extends Number> E countOdd(List<E> numbers) {
+        return null;
     }
 }
